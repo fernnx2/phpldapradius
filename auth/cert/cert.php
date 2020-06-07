e395a372bb25cff4745b0e90c2f9e3f7910100c4
fernando <devfernando@gmail.com>
2020-06-07 06:04:49 +0000

    change directory cert

--- auth/cert/cert.php
+++ auth/cert/cert.php
@@ -1,6 +1,6 @@
 <?php
-$cwd=getcwd();
 
+function generateCertificate($cwd,$useruid){
 $dn= array(
     "countryName"=>"US",
     "stateOrProvinceName" => "Santa Ana",
@@ -12,7 +12,7 @@ $dn= array(
 );
 
 $config = array(
-        'config' => $cwd.'/openssl.cnf',
+        'config' => $cwd.'/cert/openssl.cnf',
 	'encrypt_key' => false,
         'private_key_type' => OPENSSL_KEYTYPE_RSA,
         'digest_alg' => 'sha1',
@@ -33,17 +33,24 @@ $sscert = openssl_csr_sign($csr, null, $privkey, 365, $config);
 openssl_csr_export($csr, $csrout) and var_dump($csrout);
 openssl_x509_export($sscert, $certout) and var_dump($certout);
 openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
+mkdir($cwd."/cert/certs/".$useruid);
+if(openssl_x509_export_to_file($sscert, $cwd."/cert/certs/".$useruid."/clientcert.pem",FALSE)){
+	if(openssl_pkey_export_to_file($privkey, $cwd."/cert/certs/".$useruid."clientkey.key", FALSE)){
+	shell_exec("openssl x509 -outform der -in ".$cwd."/cert/certs/".$useruid."/clientcert.pem -out ".$cwd."/cert/certs/".$useruid."/clientcert.der");
+	return true;
+	}
+	else{
 
-openssl_x509_export_to_file($sscert, $cwd."/certs/mycert.pem",FALSE);
-openssl_pkey_export_to_file($privkey, $cwd."/certs/mykey.key", FALSE);
-
-var_dump(openssl_error_string());
-
+	return false;}
+}
+else{
+return false;
+}
 // Mostrar cualquier error que ocurra
 //while (($e = openssl_error_string()) !== false) {
 //    echo $e . "\n";
 //}
-
+}
 
 
 ?>
